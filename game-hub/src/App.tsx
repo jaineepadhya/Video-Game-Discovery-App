47c82c971be5149b5f155f5f6e466aec5caadc23
jaineepadhya <jaineepadhya@gmail.com>
2025-11-14 01:14:25 -0500

    Create a custom hook to fetch games

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -5,22 +5,26 @@ import GameGrid from "./components/GameGrid"
 
 function App() {
   return (
-    <Grid templateAreas={{
-    //small screens like mobile
-    base: `"nav"  "main"`,
-    //wider screens like 1024 px and above
-    lg: `"nav nav" "aside main"`
+    <Grid 
+      templateAreas={{
+      //small screens like mobile
+      base: `"nav"  "main"`,
+      //wider screens like 1024 px and above
+      lg: `"nav nav" "aside main"`
     }} >
-<GridItem area="nav">
-  <NavBar/></GridItem>
-<Show above="lg">
-<GridItem area="aside" >Aside</GridItem> 
-</Show>
+      <GridItem area="nav">
+        <NavBar/>
+      </GridItem>
+      <Show above="lg">
+      <GridItem area="aside" >
+        Aside
+        </GridItem> 
+      </Show>
       <GridItem area="main" >
-      <GameGrid/>  
+        <GameGrid/>  
       </GridItem>
     </Grid>
-    
+
 )
 }
 
