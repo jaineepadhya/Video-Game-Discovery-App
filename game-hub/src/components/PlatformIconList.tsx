312dc63095a9aba08372ffba09340f5a79a3e613
jaineepadhya <jaineepadhya@gmail.com>
2025-11-21 01:28:40 -0500

    Filter games by genre

--- game-hub/src/components/PlatformIconList.tsx
+++ game-hub/src/components/PlatformIconList.tsx
@@ -26,8 +26,8 @@ const iconMap: {[key: string]: IconType}= {
 
   return (
     <HStack marginY={1}>
-    {platforms.map((platform) => 
-    <Icon as = {iconMap[platform.slug]} color="gray.500"/>)}
+    {platforms.map((platform ) => 
+    <Icon key={platform.id} as = {iconMap[platform.slug]} color="gray.500"/>)}
     </HStack>
   )
 }
