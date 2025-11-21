69d12fa13ec456c6a45b00b33198708301d74cd8
jaineepadhya <jaineepadhya@gmail.com>
2025-11-21 00:28:27 -0500

    Display genres

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = () => {
   return (
     <>
      {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} spacing={10} padding="10px">
+    <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} spacing={3} padding="10px">
       {isLoading && Skeletons.map(Skeleton => <GameCardContainer>
         <GameCardSkeleton key={Skeleton}/>
         </GameCardContainer>)}
