c5f5df87346e350c89412fc1724cef708095b3a6
jaineepadhya <jaineepadhya@gmail.com>
2025-11-21 15:16:02 -0500

    Highlight the selected genre

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -4,11 +4,12 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props{
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
 
 
-const GenreList = ({onSelectGenre}: Props ) => {
+const GenreList = ({onSelectGenre, selectedGenre}: Props ) => {
 
    const {data, isLoading, error}=  useGenres();
 
@@ -21,6 +22,9 @@ const GenreList = ({onSelectGenre}: Props ) => {
           <HStack>
            <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/> 
            <Button 
+                fontWeight = {genre.id === selectedGenre?.id ? 'bold' : 'normal' }
+
+
                 onClick = {() => onSelectGenre(genre)} 
                 fontSize="lg" 
                 variant= "link">
