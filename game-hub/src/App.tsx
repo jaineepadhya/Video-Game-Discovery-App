551d30fedcbb6caaac4685f7560afb4a52e6d9b4
jaineepadhya <jaineepadhya@gmail.com>
2025-11-25 17:05:47 -0500

    Implemented searching

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform : Platform | null;
   sortOrder: string;
+  searchText: string
 }
 
 
@@ -32,7 +33,7 @@ const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
       lg: "200px 1fr"
     }}>
       <GridItem area="nav">
-        <NavBar/>
+        <NavBar onSearch= {(searchText) => setGameQuery({...gameQuery, searchText} ) }/>
       </GridItem>
       <Show above="lg">
       <GridItem area="aside" paddingX={5} >
